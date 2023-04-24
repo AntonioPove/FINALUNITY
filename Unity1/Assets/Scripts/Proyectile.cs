c222da8cbdce4e2b0bd17ea2a768180b1b81a874
RaulSaavedraRiera <rsaave02@ucm.es>
2023-04-24 10:42:50 +0200

    añadida pistola

---
+++ Unity1/Assets/Scripts/Proyectile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectile : MonoBehaviour
+{
+    [SerializeField]
+    float vel;
+    [SerializeField]
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb.velocity = (transform.position - Camera.main.transform.position) * vel * Time.fixedDeltaTime;
+        transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var c = collision.gameObject.GetComponent<DestroyOnCollision>();
+
+        if (c != null)
+            c.DestroyPerHit();
+
+        Destroy(gameObject);
+    }
+
+}
---
+++ Unity1/Assets/Scripts/Shoot.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField]
+    float launchVel;
+    [SerializeField]
+    GameObject proyectile;
+    [SerializeField]
+    Transform target;
+
+    bool shooting = false;
+
+    Quaternion iniRot;
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        iniRot = transform.localRotation;
+    }
+
+    
+    private void LateUpdate()
+    {
+        if (shooting)
+        {
+            transform.localRotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+        }
+    }
+
+
+    void ShootP()
+    {
+        Instantiate<GameObject>(proyectile, target.position, Quaternion.LookRotation(transform.forward));
+    }
+
+
+    public void ChangeShoot()
+    {
+        shooting = !shooting;
+
+        if (!shooting)
+        {
+            CancelInvoke();
+            animator.enabled = true;
+            transform.localRotation = iniRot;
+        }
+        else
+        {
+            animator.enabled = false;
+          
+            InvokeRepeating("ShootP", 0, launchVel);
+        }
+
+       
+    }
+}
